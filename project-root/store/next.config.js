a6634d33e504a9f1084676ceadf6a1b451f94e54
vendaerevendasdhd <vendaerevendasdhd@gmail.com>
2025-03-14 22:21:27 -0300

    Add project-root/store/next.config.js 1742001686927

---
+++ project-root/store/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['via.placeholder.com'], // Add other image domains as needed
+  },
+}
+
+module.exports = nextConfig
